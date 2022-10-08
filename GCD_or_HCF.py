cee7be29a5faa787272b621b4f7281cc907b75d1
21A91A0487 <98595776+21A91A0487@users.noreply.github.com>
2022-10-08 11:23:31 +0530

    def hcfnaive(a,b):     if(b==0):         return abs(a)     else:         return hcfnaive(b,a%b) N,M=map(int,input().split()) print(hcfnaive(N,M))

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,7 +1,7 @@
-N,M=map(int,input().split())
 def hcfnaive(a,b):
     if(b==0):
         return abs(a)
     else:
-        return hcfnaive(b, a%b)
+        return hcfnaive(b,a%b)
+N,M=map(int,input().split())
 print(hcfnaive(N,M))
