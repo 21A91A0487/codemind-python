c872c1e3d7598773d4a472082cbd838f8ee473f1
21A91A0487 <98595776+21A91A0487@users.noreply.github.com>
2023-06-27 14:53:45 +0530

    n,m = map(int,input().split()) mat = [] for i in range(n):     lst = list(map(int,input().split()))     mat.append(lst) se = 0 so = 0 for i in range(n):     for j in range(m):         if i ==  0 or i %2 == 0:             se += mat[i][j]         else:             so += mat[i][j] print(se,so)

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
@@ -0,0 +1,14 @@
+n,m = map(int,input().split())
+mat = []
+for i in range(n):
+    lst = list(map(int,input().split()))
+    mat.append(lst)
+se = 0
+so = 0
+for i in range(n):
+    for j in range(m):
+        if i ==  0 or i %2 == 0:
+            se += mat[i][j]
+        else:
+            so += mat[i][j]
+print(se,so)
