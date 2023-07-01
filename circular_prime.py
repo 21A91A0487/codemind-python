da989b3b4c2f818321a78a9a96f69ddc9e5ab7fe
21A91A0487 <98595776+21A91A0487@users.noreply.github.com>
2023-07-01 10:54:06 +0530

    import math def isprime(num):     if num<2:         return False     for i in range(2,int(num**0.5)+1):         if num%i == 0:             return False     else:         return True n = int(input()) rev=str(n)[::-1] if isprime(int(rev)) == False and isprime(n)==True:     print("prime but not a circular prime") elif isprime(n)==True and isprime(int(rev))==True:     print('circular prime') else:     print('not prime')

--- circular_prime.py
+++ circular_prime.py
@@ -1,22 +1,19 @@
 import math
 def isprime(num):
-    sq = int(math.sqrt(num))
-    for i in range(2,sq+1):
+    if num<2:
+        return False
+    for i in range(2,int(num**0.5)+1):
         if num%i == 0:
             return False
     else:
         return True
-
 n = int(input())
-if isprime(n):
-    rev = 0
-    while n:
-        d = n%10
-        n = n//10
-        rev = rev*10+d
-    if isprime(rev) == False:
-        print("prime but not a circular prime")
-    else:
-        print("circular prime")
+rev=str(n)[::-1]
+if isprime(int(rev)) == False and isprime(n)==True:
+    print("prime but not a circular prime")
+elif isprime(n)==True and isprime(int(rev))==True:
+    print('circular prime')
 else:
-    print("not prime")
+    print('not prime')
+
+        
