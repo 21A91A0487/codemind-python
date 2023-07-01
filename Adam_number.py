57b6146a06fbc1e2475a2e22415882c5005a5481
21A91A0487 <98595776+21A91A0487@users.noreply.github.com>
2023-07-01 22:43:32 +0530

    n = int(input()) a = str(n**2) b = str(n)[::-1] rev = str(int(b)**2) if a[::-1] == rev:     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,8 +1,8 @@
 n = int(input())
-s = str(n**2)
-r = str(n)[::-1]
-rev = str(int(r)**2)
-if s[::-1] == rev:
+a = str(n**2)
+b = str(n)[::-1]
+rev = str(int(b)**2)
+if a[::-1] == rev:
     print(True)
 else:
     print(False)
