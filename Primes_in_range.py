5d5ce9bcec4c2d21986d295873da2f503cdd6425
21A91A0487 <98595776+21A91A0487@users.noreply.github.com>
2023-07-03 16:04:15 +0530

    n = int(input()) m = int(input()) l = [] for num in range(n,m+1):     if num>1:         for i in range(2,int(num**0.5)+1):             if num%i == 0:                 break         else:             l.append(num) print(len(l))

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -2,7 +2,7 @@ n = int(input())
 m = int(input())
 l = []
 for num in range(n,m+1):
-    if num > 1:
+    if num>1:
         for i in range(2,int(num**0.5)+1):
             if num%i == 0:
                 break
