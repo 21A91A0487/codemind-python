7099dc1d9dc940802797ce1627ddc83ff55280d8
21A91A0487 <98595776+21A91A0487@users.noreply.github.com>
2023-07-10 23:27:44 +0530

    def prime(k):     if k == 1:         return False     else:         for i in range(2,int(k**0.5)+1):             if k%i == 0:                 return False                 break         else:             return True n = int(input()) s = [int(i) for i in str(n)] c = 0 if prime(n):     for i in s:         if prime(int(i)):             c += 1     if c == len(s):         print("Mega Prime")     else:         print("Not Mega Prime") else:     print("Not Mega Prime")

---
+++ mega_prime.py
@@ -0,0 +1,23 @@
+def prime(k):
+    if k == 1:
+        return False
+    else:
+        for i in range(2,int(k**0.5)+1):
+            if k%i == 0:
+                return False
+                break
+        else:
+            return True
+n = int(input())
+s = [int(i) for i in str(n)]
+c = 0 
+if prime(n):
+    for i in s:
+        if prime(int(i)):
+            c += 1
+    if c == len(s):
+        print("Mega Prime")
+    else:
+        print("Not Mega Prime")
+else:
+    print("Not Mega Prime")
